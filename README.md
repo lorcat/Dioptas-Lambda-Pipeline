e0a6aff29cd1e9983e2dc9d2cc78715b30dbdb36
Konstantin Glazyrin <5584235+lorcat@users.noreply.github.com>
2021-05-08 12:37:48 +0200

    Update for the new dioptas-pipeline and the initial batchfit version

--- README.md
+++ README.md
@@ -10,11 +10,19 @@ The most recent compilation of the windows distributive can be found [here](http
 The [code](https://github.com/lorcat/Dioptas-Lambda-Pipeline/blob/main/maxwell_installer/install_dioptas.py) provided inside the folder **[maxwell_installer](https://github.com/lorcat/Dioptas-Lambda-Pipeline/tree/main/maxwell_installer)** can be downloaded and executed via
 
 	python3 install_dioptas.py
- 
+
+
+A more difficult way would be:
+
+    cd ~/Downloads
+    git clone https://github.com/lorcat/Dioptas-Lambda-Pipeline
+    cd Dioptas-Lambda-Pipeline
+    python3 install_dioptas.py
+
 It will setup user links to the /gpfs data and the Dioptas on the virtual desktop of the user run.
 
-## Installation via conda
-The code can be installed via [Anaconda](https://www.anaconda.com/). At the moment installation channels for Windows and Linux are available. The following lines indicate code which should be run on the command line.
+## Installation of special Dioptas version via conda
+This dioptas version is developed by Mikhail Karnevsky. The code can be installed via [Anaconda](https://www.anaconda.com/). At the moment installation channels for Windows and Linux are available. The following lines indicate code which should be run on the command line.
 
 	conda create -n dioptas_lambda_pipeline integration_pipeline -c karnevskiy -c cprescher --yes
 	conda activate dioptas_lambda_pipeline
--- maxwell_installer/install_dioptas.py
+++ maxwell_installer/install_dioptas.py
@@ -4,58 +4,85 @@ import sys
 import subprocess
 import re
 
+
 class Installer(object):
-    DESKTOP_FILE="""
+    DESKTOP_FILE_BATCHFIT = """
 [Desktop Entry]
 Version=1.0
 Type=Application
 Terminal=false
-Exec=/asap3/fs-sc/gpfs/common/scdd01/conda_env/integration_pipeline_stable/bin/dioptas
-Name=Dioptas with Pipeline
-Icon=/asap3/fs-sc/gpfs/common/scdd01/conda_env/integration_pipeline_stable/lib/python3.6/site-packages/dioptas/resources/icons/icon.png
+Exec=/usr/bin/bash -i /asap3/fs-sc/gpfs/common/scdd01/conda_env/batchfit/bin/activate_batchfit.sh
+Name=P02.2 batch fit
+Icon=/asap3/fs-sc/gpfs/common/scdd01/conda_env/batchfit/bin/batch_fit.png"
 Categories=Science;
     """
-    
-    LINK_NAME = "Dioptas.desktop"
-    
+
+    DESKTOP_FILE_DIOPTAS = """
+[Desktop Entry]
+Version=1.0
+Type=Application
+Terminal=false
+Exec=/usr/bin/bash -i /asap3/fs-sc/gpfs/common/scdd01/conda_env/batchfit/bin/activate_batchfit.sh
+Name=P02.2 batch fit
+Icon=/asap3/fs-sc/gpfs/common/scdd01/conda_env/batchfit/bin/dioptas_pipeline.png
+Categories=Science;
+        """
+
+    LINK_NAME_DIOPTAS = "Dioptas_mkarnevsky.desktop"
+    LINK_NAME_BATCHFIT = "Batchfit_mkarnevsky.desktop"
+
+    KEY_USERHOME = "HOME"
+
     def __init__(self):
+        """
+        Initialization
+        """
         object.__init__(self)
         self.user_id = None
-        
-        self.get_userid()
-        self.create_dioptas_link()
-        self.create_data_links()
-        
-    def get_userid(self):
-        r = subprocess.run(["id"], stdout=subprocess.PIPE)
-        d = r.stdout.decode()
-        p = re.compile("uid=[0-9]+\(([^\)]+)\)")
-        
-        tl = p.findall(d)
-        if len(tl)>0:
-            self.user_id = tl[0]
-        
-    def create_dioptas_link(self):
-        
-        try:    
-            fn = os.path.join("/home/{}/Desktop".format(self.user_id), self.LINK_NAME)
+
+        setup = {self.LINK_NAME_DIOPTAS: self.DESKTOP_FILE_DIOPTAS, self.LINK_NAME_BATCHFIT: self.DESKTOP_FILE_BATCHFIT}
+
+        for (link, file_content) in setup.items():
+            self.create_desktop_link(link, file_content)
+
+        # creates a link to users data
+        self.create_data_link()
+
+    def create_desktop_link(self, link, content):
+        """
+        Creates desktop links using a filename and content
+        :param link:
+        :param content:
+        :return:
+        """
+        try:
+            base_dir = os.path.join(os.getenv(self.KEY_USERHOME), 'Desktop')
+
+            if not os.path.exists(base_dir):
+                os.makedirs(base_dir)
+
+            fn = os.path.join(base_dir, link)
             print("Creating a desktop link ({})..".format(fn))
             with open(fn, "w") as fh:
-                fh.write(self.DESKTOP_FILE)
-                
+                fh.write(content)
+
         except IOError as e:
             print("Error: {}".format(e))
-            
-    def create_data_links(self):
-        fn = "/home/{}/Desktop/data".format(self.user_id)
+
+    def create_data_link(self):
+        """
+        Creates a link to user's data simplifying navigation
+        :return:
+        """
+
+        fn = os.path.join(os.getenv(self.KEY_USERHOME), 'Desktop', 'P02.2_data')
         start = "/asap3/petra3/gpfs/p02.2"
-        print("Creating a data link ({} -> {})..".format(start,fn))
+        print("Creating a data link ({} -> {})..".format(start, fn))
         subprocess.run(["ln", "-s", start, fn])
-    
-        
+
 
 def main():
     i = Installer()
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
