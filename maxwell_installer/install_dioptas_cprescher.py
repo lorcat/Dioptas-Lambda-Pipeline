df700a84741f648cacdc773b09d25d04a4126395
Konstantin Glazyrin <5584235+lorcat@users.noreply.github.com>
2021-11-04 14:17:16 +0100

    Adding a Clemens Prescher Dioptas installation link

--- maxwell_installer/install_dioptas_cprescher.py
+++ maxwell_installer/install_dioptas_cprescher.py
@@ -49,11 +49,11 @@ Categories=Science;
             fn = os.path.join(base_dir, link)
 
             breate = True
-            if os.path.exists(fn) and os.path.islink(fn):
+            if os.path.exists(fn) and os.path.isfile(fn):
                 print("Deleting an old link ({})".format(fn))
                 self.delete_link(fn)
             elif os.path.exists(fn):
-                print("Error: ({}) exists and it is not a link".format(fn))
+                print("Error: ({}) exists and it is not a file/link".format(fn))
                 bcreate = False
 
             if breate:
