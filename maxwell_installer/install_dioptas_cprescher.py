1419de1bcb585b2c630dc45e0379c3da6e80b687
Konstantin Glazyrin <5584235+lorcat@users.noreply.github.com>
2022-08-12 13:19:00 +0200

    Adding latest Dioptas option - 0.5.4. It will be easier to switch next time to a newer version on Maxwell.

--- maxwell_installer/install_dioptas_cprescher.py
+++ maxwell_installer/install_dioptas_cprescher.py
@@ -11,9 +11,9 @@ class Installer(object):
 Version=0.5.3
 Type=Application
 Terminal=false
-Exec=/asap3/fs-sc/gpfs/common/scdd01/dioptas/dioptas-0.5.3a/Dioptas
+Exec=/asap3/fs-sc/gpfs/common/scdd01/dioptas/latest/Dioptas
 Name=Original Dioptas
-Icon=/asap3/fs-sc/gpfs/common/scdd01/dioptas/dioptas-0.5.3a/dioptas/resources/icons/icon.png
+Icon=/asap3/fs-sc/gpfs/common/scdd01/dioptas/latest/dioptas/resources/icons/icon.png
 Categories=Science;
         """
 
